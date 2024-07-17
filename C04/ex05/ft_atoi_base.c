047b51a439b31637dd807cb04a2cbb557bcb8ae8
El Madani <123333191+Madani00@users.noreply.github.com>
2024-07-17 14:40:26 +0100

    Add files via upload

---
+++ C04/ex05/ft_atoi_base.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eamchart <eamchart@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 09:28:54 by eamchart          #+#    #+#             */
+/*   Updated: 2024/07/17 14:38:32 by eamchart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	check_base(char *base)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == '-' || base[i] == '+' || \
+				(base[i] <= 13 && base[i] >= 9) || base[i] == 32)
+			return (0);
+		j = i + 1;
+		while (base[j])
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	indexof_base(char c, char *base)
+{
+	int	i;
+
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == c)
+			return (i);
+		i++;
+	}
+	return (0);
+}
+
+int	check_if(char c, char *base)
+{
+	int	i;
+
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_atoi_base(char *str, char *base)
+{
+	int	i;
+	int	op;
+	int	res;
+
+	i = 0;
+	op = 1;
+	res = 0;
+	if (!check_base(base))
+		return (0);
+	while ((str[i] <= 13 && str[i] >= 9) || (str[i] == 32))
+	{
+		i++;
+	}
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			op *= -1;
+		i++;
+	}
+	while (check_if(str[i], base))
+	{
+		res = res * ft_strlen(base) + indexof_base(str[i], base);
+		i++;
+	}
+	return (res * op);
+}
