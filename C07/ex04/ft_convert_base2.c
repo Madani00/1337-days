8f190a4848f7e92f27a9230afa864702b0156f9d
El Madani <123333191+Madani00@users.noreply.github.com>
2024-07-26 00:18:28 +0100

    Create ft_convert_base2.c

---
+++ C07/ex04/ft_convert_base2.c
@@ -0,0 +1,59 @@
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+int	nbr_length(int nbr, char *base, int length)
+{
+	int	base_length;
+	int	n;
+
+	base_length = ft_strlen(base);
+	n = nbr;
+	if (nbr < 0)
+	{
+		n = nbr * -1;
+		length++;
+	}
+	while (n >= base_length)
+	{
+		n /= base_length;
+		length++;
+	}
+	length++;
+	return (length);
+}
+
+void	ft_putnbr_base(int nbr, char *base, char *str)
+{
+	int		length_base;
+	long	nb;
+	int		i;
+	int		nbr_len;
+
+	length_base = ft_strlen(base);
+	nbr_len = nbr_length(nbr, base, 0) - 1;
+	nb = nbr;
+	i = 0;
+	if (nb < 0)
+	{
+		str[0] = '-';
+		nb *= -1;
+		i = 1;
+	}
+	while (nb >= length_base)
+	{
+		str[nbr_len] = base[nb % length_base];
+		nb /= length_base;
+		nbr_len--;
+	}
+	if (nb < length_base)
+		str[i] = base[nb];
+}
