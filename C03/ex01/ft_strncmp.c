7cfe79d6f7e86ccd0e060cb751ddb02bbfcd7548
El Madani <123333191+Madani00@users.noreply.github.com>
2024-10-26 14:21:52 +0100

    Update ft_strncmp.c

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -10,25 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
 
 	i = 0;
-	while (s1[i] && s2[i] && n)
+	while ((s1[i] || s2[i]) && i < n)
 	{
-		if (s1[i] == s2[i])
-		{
-			i++;
-			n--;
-		}
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
 	}
-	if (n == 0)
-	{
-		return (0);
-	}
-	else
-	{
-		return (s1[i] - s2[i]);
-	}
+	return (0);
 }
