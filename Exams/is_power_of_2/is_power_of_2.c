a707d1a7f0bafab4108e23772892bfdfd9aa826c
El Madani <123333191+Madani00@users.noreply.github.com>
2024-12-18 10:35:55 +0100

    Update is_power_of_2.c

--- Exams/is_power_of_2/is_power_of_2.c
+++ Exams/is_power_of_2/is_power_of_2.c
@@ -4,7 +4,7 @@ int is_power_of_2(unsigned int n)
 {
     unsigned int power_of_2 = 1;
     
-    while (power_of_2 < n && power_of_2 < 2147483648)
+    while (power_of_2 < n)               // opt : && power_of_2 < 2147483648
         power_of_2 *= 2;
     return (n == power_of_2);
 }
