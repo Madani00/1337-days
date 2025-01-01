9f901ae8b46ff08aa96f87607a59103d381da7f7
El Madani <123333191+Madani00@users.noreply.github.com>
2025-01-01 18:46:57 +0100

    Update is_power_of_2.c

--- Exams/is_power_of_2/is_power_of_2.c
+++ Exams/is_power_of_2/is_power_of_2.c
@@ -1,14 +1,24 @@
-#include <unistd.h>
-#include <stdio.h>
+// #include <unistd.h>
+// #include <stdio.h>
+// int is_power_of_2(unsigned int n)
+// {
+//     if (n == 0)
+//	  return 0;
+//     unsigned int power_of_2 = 1;
+    
+//     while (power_of_2 < n)               // opt : && power_of_2 < 2147483648
+//         power_of_2 *= 2;
+//     return (n == power_of_2);
+// }
+// int main()
+// {
+// 	printf("%d\n", is_power_of_2(15));
+// }
+
+// this one passed the molinute
 int is_power_of_2(unsigned int n)
 {
-    unsigned int power_of_2 = 1;
-    
-    while (power_of_2 < n)               // opt : && power_of_2 < 2147483648
-        power_of_2 *= 2;
-    return (n == power_of_2);
-}
-int main()
-{
-	printf("%d\n", is_power_of_2(15));
+    if (n == 0)
+        return 0;
+    return (n & (n - 1)) == 0;
 }
