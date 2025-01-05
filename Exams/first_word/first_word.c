8bf8761fa28ada5ee0efc7413f21df47c77c1d5d
El Madani <123333191+Madani00@users.noreply.github.com>
2025-01-05 22:33:16 +0100

    Update first_word.c

--- Exams/first_word/first_word.c
+++ Exams/first_word/first_word.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
 		{
 			while (argv[1][i] == ' ' || argv[1][i] == '\t')
 				i++;
-			while ((argv[1][i] != ' ' || argv[1][i] != '\t') && argv[1][i] != '\0')
+			while ((argv[1][i] != ' ' && argv[1][i] != '\t') && argv[1][i] != '\0')
 			{
 				write(1, &argv[1][i], 1);
 				i++;
