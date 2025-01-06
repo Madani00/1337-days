7b4250c6b83ad487f51a90dbb3f41cb77496fad3
El Madani <123333191+Madani00@users.noreply.github.com>
2025-01-06 18:54:31 +0100

    Update expand_str.c

--- Exams/expand_str/expand_str.c
+++ Exams/expand_str/expand_str.c
@@ -7,25 +7,26 @@ int main(int argc, char **argv)
 	char *ptr = argv[1];
 	if (argc == 2)
 	{
+		int yes;
+		int y2;
 		while (*ptr)
 		{
-			while (*ptr &&  (*ptr == 32 || (*ptr >= 9 && *ptr <= 13)))
-			{
+			yes = 0;
+			while (*ptr && (*ptr == 32 || (*ptr >= 9 && *ptr <= 13)))
 				ptr++;
-			}
-			if (*ptr)
+			while (*ptr && !(*ptr == 32 || (*ptr >= 9 && *ptr <= 13)))
 			{
-				while (*ptr && !(*ptr == 32 || (*ptr >= 9 && *ptr <= 13)))
-				{
-   		 			write(1, ptr, 1);
-					ptr++;
-				}
-				write(1, " ", 1);
-				write(1, " ", 1);
-				write(1, " ", 1);
+   		 		write(1, ptr, 1);
+				ptr++;
+				yes = 1;
 			}
+			while (*ptr && (*ptr == 32 || (*ptr >= 9 && *ptr <= 13)))
+				ptr++;
+			if (yes && *ptr)
+				write(1, "   ", 3);
 		}
-	} 
+	}
 	write(1, "\n", 1);
 	return (0);
 }
+
