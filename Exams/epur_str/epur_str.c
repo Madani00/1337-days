63be170cb674eb85465340c09530c4eca9fa1d40
El Madani <123333191+Madani00@users.noreply.github.com>
2025-01-06 18:38:33 +0100

    Update epur_str.c

--- Exams/epur_str/epur_str.c
+++ Exams/epur_str/epur_str.c
@@ -3,25 +3,25 @@
 
 int main(int argc, char **argv)
 {
-	
+
 	char *ptr = argv[1];
 	if (argc == 2)
 	{
+		int yes = 0;
 		while (*ptr)
 		{
-			while (*ptr &&  (*ptr == 32 || (*ptr >= 9 && *ptr <= 13)))
+			while (*ptr && (*ptr == 32 || (*ptr >= 9 && *ptr <= 13)))
+				ptr++;
+			while (*ptr && !(*ptr == 32  || (*ptr >= 9 && *ptr <= 13)))
 			{
+   		 		write(1, ptr, 1);
+				yes = 1;
 				ptr++;
 			}
-			if (*ptr)
-			{
-				while (*ptr && !(*ptr == 32 || (*ptr >= 9 && *ptr <= 13)))
-				{
-   		 			write(1, ptr, 1);
-					ptr++;
-				}
+			while (*ptr && (*ptr == 32 || (*ptr >= 9 && *ptr <= 13)))
+				ptr++;
+			if (*ptr && yes)
 				write(1, " ", 1);
-			}
 		}
 	}
 	write(1, "\n", 1);
